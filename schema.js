fe7a8b42a26df41673095b92fbf4554ea3c3f680
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-22 15:14:03 +0530

    schema validation usin joi

---
+++ schema.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+const listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.allow()
+  }).required(),
+});
+
+export default listingSchema;
