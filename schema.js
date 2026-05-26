b6a3013544ccb346f89316400c6682bd7ab0e77d
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-26 14:02:43 +0530

    described schema

--- schema.js
+++ schema.js
@@ -13,8 +13,8 @@ const listingSchema = Joi.object({
 
 const reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required(),
-    Comment: Joi.string().required(),
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
   }).required(),
 });
 
