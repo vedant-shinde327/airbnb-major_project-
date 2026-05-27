eae17662f4ab7fa2b3420aeebf1c8f91c3ab39e8
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-27 15:37:58 +0530

    special route for /reviews

---
+++ routes/review.js
@@ -0,0 +1,48 @@
+import express from "express";
+const router = express.Router({mergeParams: true});
+import { reviewSchema } from "../schema.js";
+import Review from "../models/review.js";
+import wrapAsync from "../utils/wrapAsync.js";
+import ExpressError from "../utils/ExpressError.js";
+import Listing from "../models/listing.js"; 
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
+
+//delete revie route
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  }),
+);
+
+export default router;
