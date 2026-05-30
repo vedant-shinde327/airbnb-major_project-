c1118dc0f609e9a2d1ba68b911766f99d26f5649
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-05-30 23:34:35 +0530

    user schema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,12 @@
-import { string } from "joi";
-import mongoose, { Schema, trusted } from "mongoose";
-const schema = mongoose.Schema;
+import mongoose from "mongoose";
 import passportLocalMongoose from "passport-local-mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: string,
-        required: true,
-    }
+  email: {
+    type: String,
+    required: true,
+  },
 });
+userSchema.plugin(passportLocalMongoose.default);
 
-User.plugin(passportLocalMongoose);
 export default mongoose.model("User", userSchema);
