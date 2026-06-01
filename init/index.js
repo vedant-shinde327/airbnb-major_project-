50355d0bdd7107058da29d65e3061f443dd93c26
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-01 09:09:32 +0530

    adding MVC design

--- init/index.js
+++ init/index.js
@@ -15,7 +15,11 @@ async function main() {
 }
 
 const initDB = async () => {
-  Listing.deleteMany({});
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6a1b1e8f09281f2b89e46734",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
