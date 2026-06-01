b89aa0ccfd3b0ebbb35e599172822babb4d4a3b5
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-01 09:07:42 +0530

    adding MVC design

--- routes/review.js
+++ routes/review.js
@@ -1,51 +1,26 @@
 import express from "express";
 const router = express.Router({mergeParams: true});
-import { reviewSchema } from "../schema.js";
 import Review from "../models/review.js";
 import wrapAsync from "../utils/wrapAsync.js";
 import ExpressError from "../utils/ExpressError.js";
 import Listing from "../models/listing.js"; 
+import { validateReview, isLoggedIn, isReviewAuthor } from "../middleware.js";
+import { createReview, deleteReview } from "../controllers/reviews.js";
 
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
+//create review
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New Review Created");
-
-    res.redirect(`/listings/${listing._id}`);
-  }),
+  wrapAsync(createReview)
 );
 
 //delete revie route
 router.delete(
   "/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-
-    req.flash("success", "Review Deleted");
-
-    res.redirect(`/listings/${id}`);
-  }),
+  isLoggedIn, 
+  isReviewAuthor, 
+  wrapAsync(deleteReview)
 );
 
 export default router;
