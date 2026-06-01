b0fadff99697f255ef32b717ba7fa110e1cc550f
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-01 22:15:22 +0530

    editing routes for cloud services

--- routes/listings.js
+++ routes/listings.js
@@ -5,27 +5,47 @@ import wrapAsync from "../utils/wrapAsync.js";
 import review from "../models/review.js";
 import { isLoggedIn, isOwner, validateListing } from "../middleware.js";
 import User from "../models/user.js";
-import { createListing, index, renderNewForm, showListing, editListing, updateListing, deleteListing } from "../controllers/listings.js";
+import {
+  createListing,
+  index,
+  renderNewForm,
+  showListing,
+  editListing,
+  updateListing,
+  deleteListing,
+} from "../controllers/listings.js";
+import multer from "multer";
+import {cloudinary, storage} from '../cloudConfig.js';
 
-//index route
-router.get("/", wrapAsync(index));
+const upload = multer({ storage });
 
-//new route
+router
+  .route("/")
+  .get(wrapAsync(index))
+  .post(
+    isLoggedIn,
+    validateListing,
+    upload.single("listing[image]"),
+    wrapAsync(createListing),
+);
+
+  //new route
 router.get("/new", isLoggedIn, wrapAsync(renderNewForm));
 
-//show routes
-router.get("/:id", wrapAsync(showListing));
-
-//create route
-router.post("/", validateListing, wrapAsync(createListing));
+router
+  .route("/:id")
+  .get(wrapAsync(showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    updateListing,
+    validateListing,
+    wrapAsync(updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(deleteListing));
 
 //edit route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(editListing));
-
-//update route
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(updateListing));
-
-//delete routes
-router.delete( "/:id", isLoggedIn, isOwner, wrapAsync(deleteListing));
 
 export default router;
