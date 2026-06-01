2ffcf9f89d6810c18499d2aa7e6b6fd163122241
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-01 22:15:49 +0530

    added cloud services

--- routes/user.js
+++ routes/user.js
@@ -7,26 +7,22 @@ import User from "../models/user.js";
 import {  saveRedirectUrl } from "../middleware.js";
 import { signUp, rendersignUpForm, renderloginForm, login, logout } from "../controllers/users.js";
 
-router.get("/signup", (rendersignUpForm));
+router
+  .route("/signup")
+  .get( rendersignUpForm)
+  .post( wrapAsync(signUp));
 
-//signup router
-router.post(
-  "/signup",
-  wrapAsync(signUp)
-);
-
-router.get("/login", (renderloginForm));
-
-//login
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  login
-);
+router
+  .route("/login")
+  .get(renderloginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    login,
+  );
 
 //logout
 router.get("/logout", (logout));
