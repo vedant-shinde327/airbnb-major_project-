8d64e7514119fc2d1641611a61df8ac3c61c0e39
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-01 22:10:01 +0530

    connecting to cloud

---
+++ cloudConfig.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "wanderlust_DEV",
+    allowed_formats: ["png", "jpeg", "jpg"],
+  },
+});
+
+export { cloudinary, storage };
