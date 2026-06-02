b39955a7c1ee4af99e361d91b7b8b50448856550
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-06-02 18:32:09 +0530

    reinitialized data for map

--- init/index.js
+++ init/index.js
@@ -1,6 +1,16 @@
 import mongoose, { trusted } from "mongoose";
 import initData from "./data.js";
 import Listing from "../models/listing.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
+console.log(process.env.MAP_TOKEN);
+
+import mbxGeocoding from "@mapbox/mapbox-sdk/services/geocoding.js";
+
+const geocodingClient = mbxGeocoding({
+  accessToken: process.env.MAP_TOKEN,
+});
 
 main()
   .then(() => {
@@ -16,10 +26,18 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "6a1b1e8f09281f2b89e46734",
-  }));
+  for (let obj of initData.data) {
+    obj.owner = "6a1b1e8f09281f2b89e46734";
+
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: `${obj.location}, ${obj.country}`,
+        limit: 1,
+      })
+      .send();
+
+    obj.geometry = response.body.features[0].geometry;
+  }
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
